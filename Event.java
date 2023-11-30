707231d1932573ec3b5cf9d380722c13b473190c
Chelsea Maame Abena Pokuaah Owusu <92218530+Chelsea-Owusu-Pokuaah@users.noreply.github.com>
2023-11-30 19:13:43 +0000

    Add files via upload
    EventManagement class. A few changes to Event class

--- Event.java
+++ Event.java
@@ -3,7 +3,14 @@ public class Event{
     String dateOfEvent;
     String description;
     String time;
+    String status = "Pending";
 
+    public Event(String eventName, String dateOfEvent,String description,String time){
+        this.eventName= eventName;
+        this.dateOfEvent = dateOfEvent;
+        this.description = description;
+        this.time = time;
+    }
     public void setDateOfEvent(String dateOfEvent) {
         this.dateOfEvent = dateOfEvent;
     }
@@ -35,5 +42,17 @@ public class Event{
     public String getTime() {
         return time;
     }
-    
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    @Override
+    public String toString(){
+        return "Event name:"+ eventName+ " Event date:"+ "Event time:" +time+ dateOfEvent +" Description";
+    }
+
+
+    // checking if events are the same
+
 }
